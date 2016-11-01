df2d124f4007e579bc15b96fb3be13218c1a5d5b
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-11-01 16:32:23 -0500

    Final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,43 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id=>params[:id]})
+  end
 
+  def new_form
+    render ("/photos/new_form.html.erb")
+  end
+
+  def create_row
+    p=Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+  def destroy
+    i=Photo.find_by({ :id=>params[:id]})
+    i.destroy
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id=>params[:id]})
+
+  end
+
+  def update_row
+    i=Photo.find_by({ :id=>params[:id]})
+    i.caption = params[:the_caption]
+    i.source = params[:the_source]
+    i.save
+    @photo = Photo.find_by({ :id=>params[:id]})
+    redirect_to("/photos/#{@photo.id}")
+
+  end
+   
 end
